9371e923608e53a4fedd9607e2ac8f8b93df2501
ajay <ajkr195@gmail.com>
2020-06-01 21:00:01 -0400

    dual ui

--- src/main/java/com/address/book/controller/AddressController.java
+++ src/main/java/com/address/book/controller/AddressController.java
@@ -33,10 +33,24 @@ public class AddressController {
 		return "index";
 	}
 	
+	@GetMapping("/list")
+	public String showTheList(Model model, @RequestParam (defaultValue="0") int page) {
+		model.addAttribute("data", addBookRepo.findAll( PageRequest.of(page, 5, Sort.by(
+			    Order.asc("id")))));
+		model.addAttribute("currentPage", page);
+		return "index2";
+	}
+	
 	@PostMapping("/save") 
 	public String saveAddBook (Addressbook ab) {
 		addBookRepo.save(ab);
 		return "redirect:/";
+	}
+	
+	@PostMapping("/save2") 
+	public String saveAddBook2 (Addressbook ab) {
+		addBookRepo.save(ab);
+		return "redirect:/list";
 	}
 	
 	
