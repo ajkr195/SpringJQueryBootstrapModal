8711bd97e1a9f4e8714dbe9b00f5b3136b970b14
ajay <ajkr195@gmail.com>
2020-06-05 16:16:27 -0400

    triple ui

--- src/main/java/com/address/book/controller/AddressController.java
+++ src/main/java/com/address/book/controller/AddressController.java
@@ -2,17 +2,16 @@ package com.address.book.controller;
 
 import java.util.Optional;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -39,6 +38,20 @@ public class AddressController {
 			    Order.asc("id")))));
 		model.addAttribute("currentPage", page);
 		return "index2";
+	}
+	
+	@GetMapping("/list3")
+	public String showTheList3(Model model) {
+		model.addAttribute("contacts", addBookRepo.findAll());
+		return "index3";
+	}
+	
+	@RequestMapping("/contact/{id}") 
+	public String getContact (@PathVariable ("id") Long id, Model model ) {
+		
+		Optional<Addressbook> contact = addBookRepo.findById(id);
+		model.addAttribute("contact" , contact);
+		return "index3 :: modalContents";
 	}
 	
 	@PostMapping("/save") 
